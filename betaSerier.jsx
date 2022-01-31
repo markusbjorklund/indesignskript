99cdb5da47b8f702a286177204a5fb52419056d6
Markus Bjorklund <markus.bjorklund@gmail.com>
2022-01-31 22:31:48 +0200

    beta för att montera serier

---
+++ betaSerier.jsx
@@ -0,0 +1,139 @@
+﻿///////////////////////////////////////////////////////////////////////////////////////////////////
+// betaSerier
+// skript för att automatiskt montera serier på printsidor
+//
+// author Markus Björklund
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+bytSeriestrip(app.activeDocument);
+
+function bytSeriestrip(doc) {
+  var sidnummer = app.activeWindow.activePage.name;
+  var sidnamn = app.activeDocument.name;
+  var servermapp = "~/Desktop/serier/";
+  var filnamn = "ankeborg";
+
+  // ID är aldrig lika på vänster eller höger sida. ¯\_(ツ)_/¯
+
+  // vänster sida, jämnt sidnummer
+  // ÖT:s strippar
+  if (sidnummer % 2 == 0 && sidnamn.match("OTA")) {
+
+    // alert(sidnummer + " är jämnt.");
+
+    var ankeborg = doc.rectangles.itemByID(52279);
+    var ankeborg_strip = new File(servermapp + "ankeborg/" + filnamn + ".jpg");
+    ankeborg.place(ankeborg_strip);
+
+    var dragos = doc.rectangles.itemByID(52282);
+    var dragos_strip = new File(servermapp + "dragos/" + filnamn + ".jpg");
+    dragos.place(dragos_strip);
+
+    var helge = doc.rectangles.itemByID(52286);
+    var helge_strip = new File(servermapp + "helge/" + filnamn + ".jpg");
+    helge.place(helge_strip);
+
+    var medel = doc.rectangles.itemByID(52290);
+    var medel_strip = new File(servermapp + "medel/" + filnamn + ".jpg");
+    medel.place(medel_strip);
+
+    var niemi = doc.rectangles.itemByID(52295);
+    var niemi_strip = new File(servermapp + "niemi/" + filnamn + ".jpg");
+    niemi.place(niemi_strip);
+
+    var zits = doc.rectangles.itemByID(52298);
+    var zits_strip = new File(servermapp + "zits/" + filnamn + ".jpg");
+    zits.place(zits_strip);
+  }
+
+  // VBL:s strippar
+  else if (sidnummer % 2 == 0 && sidnamn.match("VBA")) {
+    var ankeborg_strip = new File(servermapp + "ankeborg/" + filnamn + ".jpg");
+    ankeborg.place(ankeborg_strip);
+
+    var hagbard = doc.rectangles.itemByID(52282);
+    var dragos_strip = new File(servermapp + "hagbard/" + filnamn + ".jpg");
+    hagbard.place(hagbard_strip);
+
+    var helge = doc.rectangles.itemByID(52286);
+    var helge_strip = new File(servermapp + "helge/" + filnamn + ".jpg");
+    helge.place(helge_strip);
+
+    var morrgan = doc.rectangles.itemByID(52290);
+    var morrgan_strip = new File(servermapp + "morrgan/" + filnamn + ".jpg");
+    morrgan.place(morrgan_strip);
+
+    var niemi = doc.rectangles.itemByID(52295);
+    var niemi_strip = new File(servermapp + "niemi/" + filnamn + ".jpg");
+    niemi.place(niemi_strip);
+
+    var zits = doc.rectangles.itemByID(52298);
+    var zits_strip = new File(servermapp + "zits/" + filnamn + ".jpg");
+    zits.place(zits_strip);
+  }
+
+  // höger sida, ojämnt sidnummer
+  // ÖT:s strippar
+  else if (sidnummer % 3 == 0 && sidnamn.match("OTA")) {
+
+    // alert(sidnummer + " är ojämnt.");
+
+    var ankeborg = doc.rectangles.itemByID(52279);
+    var ankeborg_strip = new File(servermapp + "ankeborg/" + filnamn + ".jpg");
+    ankeborg.place(ankeborg_strip);
+
+    var dragos = doc.rectangles.itemByID(52282);
+    var dragos_strip = new File(servermapp + "dragos/" + filnamn + ".jpg");
+    dragos.place(dragos_strip);
+
+    var helge = doc.rectangles.itemByID(52286);
+    var helge_strip = new File(servermapp + "helge/" + filnamn + ".jpg");
+    helge.place(helge_strip);
+
+    var medel = doc.rectangles.itemByID(52290);
+    var medel_strip = new File(servermapp + "medel/" + filnamn + ".jpg");
+    medel.place(medel_strip);
+
+    var niemi = doc.rectangles.itemByID(52295);
+    var niemi_strip = new File(servermapp + "niemi/" + filnamn + ".jpg");
+    niemi.place(niemi_strip);
+
+    var zits = doc.rectangles.itemByID(52298);
+    var zits_strip = new File(servermapp + "zits/" + filnamn + ".jpg");
+    zits.place(zits_strip);
+  }
+
+    // VBL:s strippar
+    else if (sidnummer % 3 == 0 && sidnamn.match("VBA")) {
+      var ankeborg_strip = new File(servermapp + "ankeborg/" + filnamn + ".jpg");
+      ankeborg.place(ankeborg_strip);
+  
+      var hagbard = doc.rectangles.itemByID(52282);
+      var dragos_strip = new File(servermapp + "hagbard/" + filnamn + ".jpg");
+      hagbard.place(hagbard_strip);
+  
+      var helge = doc.rectangles.itemByID(52286);
+      var helge_strip = new File(servermapp + "helge/" + filnamn + ".jpg");
+      helge.place(helge_strip);
+  
+      var morrgan = doc.rectangles.itemByID(52290);
+      var morrgan_strip = new File(servermapp + "morrgan/" + filnamn + ".jpg");
+      morrgan.place(morrgan_strip);
+  
+      var niemi = doc.rectangles.itemByID(52295);
+      var niemi_strip = new File(servermapp + "niemi/" + filnamn + ".jpg");
+      niemi.place(niemi_strip);
+  
+      var zits = doc.rectangles.itemByID(52298);
+      var zits_strip = new File(servermapp + "zits/" + filnamn + ".jpg");
+      zits.place(zits_strip);
+    }
+
+  // varning om allt skiter sig
+  else {
+    // alert(sidnummer + " är ojämnt.");
+    alert(servermapp);
+    alert(filnamn);
+  }
+}
