bfde18c9509146955432669bc83b2529e7339f83
markusbjorklund <markus.bjorklund@gmail.com>
2022-03-25 17:11:50 +0200

    ppp

---
+++ PapperPuttePaska.jsx
@@ -0,0 +1,16 @@
+﻿///////////////////////////////////////////////////////////////////////////////////////////////////
+// 
+// author Markus Björklund
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+PapperPuttePaska(app.activeDocument);
+
+function PapperPuttePaska(doc) {
+  doc.layers.everyItem().locked = false;
+
+  var sidMaterial = new Array;
+  sidMaterial = app.activeWindow.activePage.pageItems;
+  app.activeWindow.activePage.groups.add(sidMaterial);
+  app.activeWindow.activeSpread.groups.item(0).select();
+}
