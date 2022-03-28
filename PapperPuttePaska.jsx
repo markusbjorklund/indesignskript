8c3adc50eb54e5f05754ee1dc5faa61778811f08
Markus Bjorklund <markus.bjorklund@gmail.com>
2022-03-28 17:12:44 +0300

    ppp

--- PapperPuttePaska.jsx
+++ PapperPuttePaska.jsx
@@ -7,13 +7,12 @@
 PapperPuttePaska(app.activeDocument);
 
 function PapperPuttePaska(doc) {
+  app.activeDocument.save();  
   doc.layers.everyItem().locked = false;
-
   var sidMaterial = new Array;
   sidMaterial = app.activeWindow.activePage.pageItems;
   app.activeWindow.activePage.groups.add(sidMaterial);
   app.activeWindow.activeSpread.groups.item(0).select();
   app.copy();
-  app.activeDocument.save();
-  app.activeDocument.close();
+  app.activeDocument.close(SaveOptions.no);
 }
