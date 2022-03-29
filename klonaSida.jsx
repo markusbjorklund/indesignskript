03b3146e2c4b9a3c5a0b353ce1485c6450ddc6fd
Markus Bjorklund <markus.bjorklund@gmail.com>
2022-03-29 20:37:11 +0300

    sidkopiera med stil

---
+++ klonaSida.jsx
@@ -0,0 +1,18 @@
+﻿///////////////////////////////////////////////////////////////////////////////////////////////////
+// 
+// author Markus Björklund
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+klonaSida(app.activeDocument);
+
+function klonaSida(doc) {
+  app.activeDocument.save();  
+  doc.layers.everyItem().locked = false;
+  var sidMaterial = new Array;
+  sidMaterial = app.activeWindow.activePage.pageItems;
+  app.activeWindow.activePage.groups.add(sidMaterial);
+  app.activeWindow.activeSpread.groups.item(0).select();
+  app.copy();
+  app.activeDocument.close(SaveOptions.no);
+}
