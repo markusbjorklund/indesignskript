75d555c6e74ba68b800492eb516b3b7f2c3bee56
Markus Bjorklund <markus.bjorklund@gmail.com>
2022-09-13 11:24:21 +0300

    loop added

--- monteraBorsen.jsx
+++ monteraBorsen.jsx
@@ -11,18 +11,8 @@ var servermapp = "//npfileserver.tidningen.fi/";
 var foldermapp = "Diverse/EXTERNT MATERIAL/PRINTEN/borsen/";
 
 // kör in fyra börsspalter så finns det på alla mallar i paletten sidor
-var borsspalten = app.activeDocument.links.itemByName("borsen_Dummy.jpg").parent;
-var borsspalten_pdf = new File(servermapp + foldermapp + "Vasabladet.pdf");
-borsspalten.place(borsspalten_pdf);
-
-var borsspalten = app.activeDocument.links.itemByName("borsen_Dummy.jpg").parent;
-var borsspalten_pdf = new File(servermapp + foldermapp + "Vasabladet.pdf");
-borsspalten.place(borsspalten_pdf);
-
-var borsspalten = app.activeDocument.links.itemByName("borsen_Dummy.jpg").parent;
-var borsspalten_pdf = new File(servermapp + foldermapp + "Vasabladet.pdf");
-borsspalten.place(borsspalten_pdf);
-
-var borsspalten = app.activeDocument.links.itemByName("borsen_Dummy.jpg").parent;
-var borsspalten_pdf = new File(servermapp + foldermapp + "Vasabladet.pdf");
-borsspalten.place(borsspalten_pdf);
+for (int i = 0; i < 4; i++) {
+  var borsspalten = app.activeDocument.links.itemByName("borsen_Dummy.jpg").parent;
+  var borsspalten_pdf = new File(servermapp + foldermapp + "Vasabladet.pdf");
+  borsspalten.place(borsspalten_pdf);
+}
