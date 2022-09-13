ea5d5c4e65437b4ca47dc9a01821b89f639ec8c3
Markus Bjorklund <markus.bjorklund@gmail.com>
2022-09-13 11:37:00 +0300

    spelling mistake

--- monteraBorsen.jsx
+++ monteraBorsen.jsx
@@ -11,7 +11,7 @@ var servermapp = "//npfileserver.tidningen.fi/";
 var foldermapp = "Diverse/EXTERNT MATERIAL/PRINTEN/borsen/";
 
 // kör in fyra börsspalter så finns det på alla mallar i paletten sidor
-for (int i = 0; i < 4; i++) {
+for (i = 0; i < 4; i++) {
   var borsspalten = app.activeDocument.links.itemByName("borsen_Dummy.jpg").parent;
   var borsspalten_pdf = new File(servermapp + foldermapp + "Vasabladet.pdf");
   borsspalten.place(borsspalten_pdf);
