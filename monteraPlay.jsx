d6550583f0e1a46edef3bbac1de0ab4dbd82a18e
markusbjorklund <markus.bjorklund@gmail.com>
2022-09-14 09:48:16 +0300

    fix stupid

--- monteraPlay.jsx
+++ monteraPlay.jsx
@@ -18,6 +18,7 @@ var day = ("0" + play_datum.getDate()).slice(-2);
 var month = ("0" + (play_datum.getMonth() + 1)).slice(-2);
 var shortYear = ("0" + play_datum.getFullYear()).slice(-2);
 play_datum = shortYear + month + day;
+var filnamn = play_datum;
 
 var play = app.activeDocument.links.itemByName("play_Dummy.jpg").parent;
 var play_pdf = new File(servermapp + foldermapp + filnamn + "-TVRVKO-PLAY-0-0-print.pdf");
