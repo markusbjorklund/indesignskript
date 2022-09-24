07db967483addfca0069ecf56fd8fca88147ea3c
markusbjorklund <markus.bjorklund@gmail.com>
2022-09-24 12:42:57 +0300

    lagt till meddelande för error

--- monteraBorsen.jsx
+++ monteraBorsen.jsx
@@ -10,9 +10,14 @@
 var servermapp = "//serveradressen kommer hit/";
 var foldermapp = "Diverse/EXTERNT MATERIAL/PRINTEN/borsen/";
 
-// kör in fyra börsspalter så finns det på alla mallar i paletten sidor
-for (i = 0; i < 4; i++) {
-  var borsspalten = app.activeDocument.links.itemByName("borsen_Dummy.jpg").parent;
-  var borsspalten_pdf = new File(servermapp + foldermapp + "Vasabladet.pdf");
-  borsspalten.place(borsspalten_pdf);
+try {
+  // kör in fyra börsspalter så finns det på alla mallar i paletten sidor
+  for (i = 0; i < 4; i++) {
+    var borsspalten = app.activeDocument.links.itemByName("borsen_Dummy.jpg").parent;
+    var borsspalten_pdf = new File(servermapp + foldermapp + "Vasabladet.pdf");
+    borsspalten.place(borsspalten_pdf);
+  }
+}
+catch (e) {
+  alert("Börsspalten har inte kommit ännu!");
 }
