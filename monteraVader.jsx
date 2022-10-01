cbd4c09eae1b2105d74f7af2b8a24d097cc578c0
markusbjorklund <markus.bjorklund@gmail.com>
2022-10-01 19:51:04 +0300

    errormeddelande om fil saknas i mappen

--- monteraVader.jsx
+++ monteraVader.jsx
@@ -26,12 +26,24 @@ var filnamn = dagens_vader;
 if (sidnamn.match("VB") || sidnamn.match("OT")) {
     var vader = app.activeDocument.links.itemByName("vader_Dummy_.jpg").parent;
     var vader_pdf = new File(servermapp + foldermapp + filnamn + "VasaBladet14.pdf");
-    vader.place(vader_pdf);
+
+    try {
+        vader.place(vader_pdf);
+    }
+    catch (e) {
+        alert("Filen finns inte! \n\nLadda upp den manuellt i mappen EXTERNT MATERIAL/PRINTEN/vader eller montera den direkt från skrivbordet och bädda in länken.");
+    }
 }
 
 // sydin
 else if (sidnamn.match("SY")) {
     var vader = app.activeDocument.links.itemByName("fmisy_Dummy.pdf.jpg").parent;
     var vader_pdf = new File(servermapp + foldermapp + filnamn + "Syd-Osterbotten14.pdf");
-    vader.place(vader_pdf);
+
+    try { 
+        vader.place(vader_pdf);
+    }
+    catch (e) {
+        alert("Filen finns inte! \n\nLadda upp den manuellt i mappen EXTERNT MATERIAL/PRINTEN/vader eller montera den direkt från skrivbordet och bädda in länken.");
+    }
 }
