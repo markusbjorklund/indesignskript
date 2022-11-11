d3d964d8efbf483496670f641a51e5d3db370f1e
markusbjorklund <markus.bjorklund@gmail.com>
2022-11-11 15:37:35 +0200

    cleanup

--- Winston Wolf.jsx
+++ Winston Wolf.jsx
@@ -1,256 +1,165 @@
-﻿/*
-                               __
-                             .d$$b
-                           .' TO$;\
-                          /  : TP._;
-                         / _.;  :Tb|
-                        /   /   ;j$j
-                    _.-"       d$$$$
-                  .' ..       d$$$$;
-                 /  /P'      d$$$$P. |\
-                /   "      .d$$$P' |\^"l
-              .'           `T$P^"""""  :
-          ._.'      _.'                ;
-       `-.-".-'-' ._.       _.-"    .-"
-     `.-" _____  ._              .-"
-    -(.g$$$$$$$b.              .'
-      ""^^T$$$P^)            .(:
-        _/  -"  /.'         /:/;
-     ._.'-'`-'  ")/         /;/;
-  `-.-"..--""   " /         /  ;
- .-" ..--""        -'          :
- ..--""--.-"         (\      .-(\
-   ..--""              `-\(\/;`
-     _.                      :
-                             ;`-
-                            :\
-                            ;
-                            
-// --- Winston Wolf aka The Wolf --- //
+﻿//DESCRIPTION:Create a hyperlink to the URL on the clipboard for the selected text
 
-Winston Wolf, also known as "The Wolf", is a supporting character in Quentin Tarantino's second film, Pulp Fiction.
-He is a cleaner working for Marsellus Wallace and was portrayed by Harvey Keitel.
+// Modified 2021-03-24
+// Keith Gilbert, Gilbert Consulting
+// www.gilbertconsulting.com
 
-Call "The Wolf" by pressing F12.
+Pre();
+  
+function Pre () {  
+	if (app.documents.length < 1) {
+		alert("No InDesign documents are open. Please open a document and try again.");
+		return;
+	}
+	if (locateCharacterStyle("Hyperlink") == "not found") {
+		alert("This script requires a character style named 'Hyperlink'. Please create a character style named 'Hyperlink' and try again.");
+		return;
+	}
+	if(app.selection.length != 1) {
+		alert("Please select some text to which you want to add a hyperlink and try again.");
+		return;
+	}
+	switch(app.selection[0].constructor.name){
+		case "Character":
+		case "Word":
+		case "TextStyleRange":
+		case "Line":
+		case "Paragraph":
+		case "TextColumn":
+		case "Text":
+			// Main();            
+			app.doScript(Main, undefined, undefined, UndoModes.ENTIRE_SCRIPT,"Run Script");
+			break;
+        case "TextFrame":
+		case "TextBox":
+		case "Rectangle":
+		case "Oval":
+		case "Polygon":
+		case "GraphicLine":
+		case "Image":
+		case "PDF":
+		case "EPS":
+		case "Character":
+		case "Word":
+		case "TextStyleRange":
+		case "Line":
+		case "Paragraph":
+		case "TextColumn":
+		case "Text":
+		case "Table":
+		case "Cell":
+		default:
+			alert("Please select some text and try again.");
+			break;
+	}  
+}  
 
-script author Markus Bjorklund
-*/
-
-Wolf();
-
-function Wolf() {
-  if (app.documents.length < 1) {
-    alert("För att köra skriptet måste du ha en sida öppen.");
-    return;
-  }
-  else {
-
-
-    // töm sök och ersätt innan skriptet kör
-    app.findGrepPreferences = NothingEnum.nothing;
-    app.changeGrepPreferences = NothingEnum.nothing;
-
-    // fixa korrekt pratminus
-    app.findGrepPreferences.findWhat = "\\\x{D}- ";
-    app.changeGrepPreferences.changeTo = "\\\x{D}– ";
-    app.activeDocument.changeGrep();
-
-    // fixa till korrekt blanksteg efter pratminus
-    app.findGrepPreferences.findWhat = "– ";
-    app.changeGrepPreferences.changeTo = "–\\\x{2009}";
-    app.activeDocument.changeGrep();
-
-    // fixa stupid
-    app.findGrepPreferences.findWhat = " –\\\x{2009}";
-    app.changeGrepPreferences.changeTo = " – ";
-    app.activeDocument.changeGrep();
-
-    // fixa mellanslaget efter Ortnamn i text, direkt efter import från mananger
-    app.findGrepPreferences.findWhat = " \\\x{002E} ";
-    app.changeGrepPreferences.changeTo = "\\\x{002E}";
-    app.activeDocument.changeGrep();
-
-    // fixa mellanslaget efter Ortnamn i text, för artikel som finns i systemet och får ändrad layout
-    app.findGrepPreferences.findWhat = "\\\x{002E}  ";
-    app.changeGrepPreferences.changeTo = "\\\x{002E} ";
-    app.activeDocument.changeGrep();
-
-    // fixa dubbla rader på citatrutorna
-    app.findGrepPreferences.findWhat = "\\\x{A}              \\\x{D}";
-    app.changeGrepPreferences.changeTo = "";
-    app.activeDocument.changeGrep();
-
-    // fixa onödigt radbyte på tidningsbylinen – åtgärdar bugg som finns i nya NP
-    app.findGrepPreferences.findWhat = "\\\x{D}\\\x{0020}Vbl";
-    app.changeGrepPreferences.changeTo = " VBL";
-    app.activeDocument.changeGrep();
-
-    app.findGrepPreferences.findWhat = "\\\x{D}\\\x{0020}VBL";
-    app.changeGrepPreferences.changeTo = " VBL";
-    app.activeDocument.changeGrep();
-
-    app.findGrepPreferences.findWhat = "\\\x{D}\\\x{0020}Öt";
-    app.changeGrepPreferences.changeTo = " ÖT";
-    app.activeDocument.changeGrep();
-
-    app.findGrepPreferences.findWhat = "\\\x{D}\\\x{0020}ÖT";
-    app.changeGrepPreferences.changeTo = " ÖT";
-    app.activeDocument.changeGrep();
-
-    // rödflagga webbspecifika element i artikeln som kommer från managern
-    app.findGrepPreferences.findWhat = "Läs också:";
-    app.changeGrepPreferences.appliedCharacterStyle = "Bildingress ingång";
-    app.changeGrepPreferences.changeTo = "\\\x{D}Artikeln har länkat material på webben.\\\x{D}";
-    app.activeDocument.changeGrep();
-
-    app.findGrepPreferences.findWhat = "LÄS OCKSÅ:";
-    app.changeGrepPreferences.appliedCharacterStyle = "Bildingress ingång";
-    app.changeGrepPreferences.changeTo = "\\\x{D}Artikeln har länkat material på webben.\\\x{D}";
-    app.activeDocument.changeGrep();
-
-    app.findGrepPreferences.findWhat = "Artikeln uppdaterad";
-    app.changeGrepPreferences.appliedCharacterStyle = "Bildingress ingång";
-    app.changeGrepPreferences.changeTo = "\\\x{D}Artikeln har uppdaterats på webben. Kolla så den fortfarande funkar i print.\\\x{D}";
-    app.activeDocument.changeGrep();
-
-    app.findGrepPreferences.findWhat = "Artikeln är uppdaterad";
-    app.changeGrepPreferences.appliedCharacterStyle = "Bildingress ingång";
-    app.changeGrepPreferences.changeTo = "\\\x{D}Artikeln har uppdaterats på webben. Kolla så den fortfarande funkar i print.\\\x{D}";
-    app.activeDocument.changeGrep();
-
-    app.findGrepPreferences.findWhat = "Uppdaterad klockan";
-    app.changeGrepPreferences.appliedCharacterStyle = "Bildingress ingång";
-    app.changeGrepPreferences.changeTo = "\\\x{D}Artikeln har uppdaterats på webben. Kolla så den fortfarande funkar i print.\\\x{D}";
-    app.activeDocument.changeGrep();
-
-    app.findGrepPreferences.findWhat = "Uppdaterad kl";
-    app.changeGrepPreferences.appliedCharacterStyle = "Bildingress ingång";
-    app.changeGrepPreferences.changeTo = "\\\x{D}Artikeln har uppdaterats på webben. Kolla så den fortfarande funkar i print.\\\x{D}";
-    app.activeDocument.changeGrep();
-
-    app.findGrepPreferences.findWhat = "Uppdaterad kl.";
-    app.changeGrepPreferences.appliedCharacterStyle = "Bildingress ingång";
-    app.changeGrepPreferences.changeTo = "\\\x{D}Artikeln har uppdaterats på webben. Kolla så den fortfarande funkar i print.\\\x{D}";
-    app.activeDocument.changeGrep();
-
-    // töm sök och ersätt
-    app.findGrepPreferences = NothingEnum.nothing;
-    app.changeGrepPreferences = NothingEnum.nothing;
-
-    // ändra mejladresser för vasabladet
-    if (sidnamn.match("VB-") || sidnamn.match("VBA")) {
-      app.findGrepPreferences.findWhat = "@ot.fi";
-      app.changeGrepPreferences.changeTo = "@vasabladet.fi";
-      app.activeDocument.changeGrep();
-      app.findGrepPreferences.findWhat = "@sydin.fi";
-      app.changeGrepPreferences.changeTo = "@vasabladet.fi";
-      app.activeDocument.changeGrep();
-      app.findGrepPreferences.findWhat = "@hssmedia.fi";
-      app.changeGrepPreferences.changeTo = "@vasabladet.fi";
-      app.activeDocument.changeGrep();
-
-      // ändra avdelningsmejladresser för vasabladet
-      app.findGrepPreferences.findWhat = "red@vasabladet.fi";
-      app.changeGrepPreferences.changeTo = "nyheter@vasabladet.fi";
-      app.activeDocument.changeGrep();
-      app.findGrepPreferences.findWhat = "sporten@vasabladet.fi";
-      app.changeGrepPreferences.changeTo = "sport@vasabladet.fi";
-      app.activeDocument.changeGrep();
-      app.findGrepPreferences.findWhat = "otkultur@vasabladet.fi";
-      app.changeGrepPreferences.changeTo = "kultur@vasabladet.fi";
-      app.activeDocument.changeGrep();
-      app.findGrepPreferences.findWhat = "otdebatt@vasabladet.fi";
-      app.changeGrepPreferences.changeTo = "debatt@vasabladet.fi";
-      app.activeDocument.changeGrep();
-
-      // ändra telefonnummer för vasabladet
-      app.findGrepPreferences.findWhat = "06-7848 803";
-      app.changeGrepPreferences.changeTo = "06-7848 200";
-      app.activeDocument.changeGrep();
-      app.findGrepPreferences.findWhat = "06-7848 453";
-      app.changeGrepPreferences.changeTo = "06-7848 293";
-      app.activeDocument.changeGrep();
-      app.findGrepPreferences.findWhat = "06-7848 545";
-      app.changeGrepPreferences.changeTo = "06-7848 289";
-      app.activeDocument.changeGrep();
-      app.findGrepPreferences.findWhat = "06-7848 472";
-      app.changeGrepPreferences.changeTo = "06-7848 204";
-      app.activeDocument.changeGrep();
+function Main() {
+	var myDoc = app.activeDocument;
+	var myText = app.selection[0];
+    // Create a temporary layer and text frame to retrieve the clipboard contents
+    var myTempLayer = myDoc.layers.add();
+    var myTempTextFrame = myDoc.textFrames.add(myTempLayer);
+    myTempTextFrame.insertionPoints[-1].select();
+    app.paste();
+    var myURL = myTempTextFrame.parentStory.contents;
+    myTempTextFrame.remove();
+    myTempLayer.remove();
+	// If the selection already contains a hyperlink, remove it
+	myRemoveHyperlink(myDoc, myText);
+    // Create the hyperlink
+    var mySource = myDoc.hyperlinkTextSources.add(myText);
+    var myDestination = myDoc.hyperlinkURLDestinations.add(myURL, {hidden:true});
+    var myHyperLink = myDoc.hyperlinks.add(mySource,myDestination);
+    try {
+        myHyperLink.name = myText.contents;
     }
-
-    // ändra mejladresser för ot
-    else if (sidnamn.match("OT-") || sidnamn.match("OTA")) {
-      app.findGrepPreferences.findWhat = "@vasabladet.fi";
-      app.changeGrepPreferences.changeTo = "@ot.fi";
-      app.activeDocument.changeGrep();
-      app.findGrepPreferences.findWhat = "@sydin.fi";
-      app.changeGrepPreferences.changeTo = "@ot.fi";
-      app.activeDocument.changeGrep();
-      app.findGrepPreferences.findWhat = "@hssmedia.fi";
-      app.changeGrepPreferences.changeTo = "@ot.fi";
-      app.activeDocument.changeGrep();
-
-      // ändra avdelningsmejladresser för ot
-      app.findGrepPreferences.findWhat = "nyheter@ot.fi";
-      app.changeGrepPreferences.changeTo = "red@ot.fi";
-      app.activeDocument.changeGrep();
-      app.findGrepPreferences.findWhat = "sport@ot.fi";
-      app.changeGrepPreferences.changeTo = "sporten@ot.fi";
-      app.activeDocument.changeGrep();
-      app.findGrepPreferences.findWhat = "kultur@ot.fi";
-      app.changeGrepPreferences.changeTo = "otkultur@ot.fi";
-      app.activeDocument.changeGrep();
-      app.findGrepPreferences.findWhat = "debatt@ot.fi";
-      app.changeGrepPreferences.changeTo = "otdebatt@ot.fi";
-      app.activeDocument.changeGrep();
-      // take care of stupid
-      app.findGrepPreferences.findWhat = "otot";
-      app.changeGrepPreferences.changeTo = "ot";
-      app.activeDocument.changeGrep();
-
-      // ändra telefonnummer för ot
-      app.findGrepPreferences.findWhat = "06-7848 200";
-      app.changeGrepPreferences.changeTo = "06-7848 803";
-      app.activeDocument.changeGrep();
-      app.findGrepPreferences.findWhat = "06-7848 293";
-      app.changeGrepPreferences.changeTo = "06-7848 453";
-      app.activeDocument.changeGrep();
-      app.findGrepPreferences.findWhat = "06-7848 289";
-      app.changeGrepPreferences.changeTo = "06-7848 545";
-      app.activeDocument.changeGrep();
-      app.findGrepPreferences.findWhat = "06-7848 204";
-      app.changeGrepPreferences.changeTo = "06-7848 472";
-      app.activeDocument.changeGrep();
+    catch (myError) {
+        var n=1;
+        while (myHyperLink.name == "Hyperlink") {
+            try {
+                myHyperLink.name = myText.contents+" "+String(n);
+            }
+            catch (myError2) {
+                n++;	
+            }
+        }
     }
-
-    // ändra mejladresser för sydin
-    else if (sidnamn.match("SY-") || sidnamn.match("SYA")) {
-      app.findGrepPreferences.findWhat = "@vasabladet.fi";
-      app.changeGrepPreferences.changeTo = "@sydin.fi";
-      app.activeDocument.changeGrep();
-      app.findGrepPreferences.findWhat = "@ot.fi";
-      app.changeGrepPreferences.changeTo = "@sydin.fi";
-      app.activeDocument.changeGrep();
-      app.findGrepPreferences.findWhat = "@hssmedia.fi";
-      app.changeGrepPreferences.changeTo = "@sydin.fi";
-      app.activeDocument.changeGrep();
+    myText.appliedCharacterStyle = myDoc.characterStyles.itemByID(locateCharacterStyle("Hyperlink"));
+}
+// Find a character style possibly contained inside a style group. Return id of the character style.
+function locateCharacterStyle(styleNameToLocate, pathSeparator) {
+	var myDoc = app.activeDocument;
+    var rootPath = "";
+    if (pathSeparator !== undefined) {
+        rootPath = pathSeparator;
     }
+	var retVal = locateCharacterStyleRecursive(myDoc, styleNameToLocate, pathSeparator, rootPath);
+	if (! retVal) {
+		return "not found";
+	}
+	return retVal.id;
+}
+// Find character styles that are in style groups. Returns the character style or null if not found.
+function locateCharacterStyleRecursive(characterStyleContainer, styleNameToLocate, pathSeparator, parentPath) {
+	var retVal = null;
+	do { 
+		try {
+			var characterStyles = characterStyleContainer.characterStyles;
+			var numCharacterStyles = characterStyles.length; 
+			for (
+				var characterStyleIdx = 0; 
+				retVal == null && characterStyleIdx < numCharacterStyles;
+				characterStyleIdx++) {
+				var characterStyle = characterStyles.item(characterStyleIdx);
+				if (characterStyle.isValid) {
+					var characterStyleName = characterStyle.name;
+                      if (pathSeparator) {
+                          characterStyleName = parentPath + characterStyleName;
+                      }
+					if (characterStyleName == styleNameToLocate) {
+						retVal = characterStyle;
+					}
+				}
+			}
+			if (retVal) {
+				break;
+			}
+			var characterStyleGroups = characterStyleContainer.characterStyleGroups;
+			var numCharacterStyleGroups = characterStyleGroups.length;
+			for (
+				var characterStyleGroupIdx = 0; 
+				retVal == null && characterStyleGroupIdx < numCharacterStyleGroups;
+				characterStyleGroupIdx++) {
 
-    // ändra mejladresser för bilagor
-    else if (sidnamn.match("VBT1-") || sidnamn.match("OTT1-") || sidnamn.match("SYTT1-")) {
-      app.findGrepPreferences.findWhat = "@vasabladet.fi";
-      app.changeGrepPreferences.changeTo = "@hssmedia.fi";
-      app.activeDocument.changeGrep();
-      app.findGrepPreferences.findWhat = "@ot.fi";
-      app.changeGrepPreferences.changeTo = "@hssmedia.fi";
-      app.activeDocument.changeGrep();
-      app.findGrepPreferences.findWhat = "@sydin.fi";
-      app.changeGrepPreferences.changeTo = "@hssmedia.fi";
-      app.activeDocument.changeGrep();
-    }
-
-    // töm sök och ersätt innan skriptet avslutas
-    app.findGrepPreferences = NothingEnum.nothing;
-    app.changeGrepPreferences = NothingEnum.nothing;
-  }
+				var characterStyleGroup = characterStyleGroups.item(characterStyleGroupIdx);
+				if (characterStyleGroup.isValid) {
+					var path = parentPath;
+                      if (pathSeparator) {
+                          var characterStyleGroupName = characterStyleGroup.name;
+                          path += characterStyleGroupName + pathSeparator;
+                      }
+					retVal = locateCharacterStyleRecursive(characterStyleGroup, styleNameToLocate, pathSeparator, path);
+				}
+			}
+		}
+		catch (err) {
+		}
+	}
+	while (false);
+	return retVal;
+}
+// Remove the hyperlink whose source is the specified text
+function myRemoveHyperlink(myDoc,myText) {
+	for (var j = myDoc.hyperlinks.length-1; j >= 0; j--) {
+		var mySourceBegin = myDoc.hyperlinks[j].source.sourceText.insertionPoints[0].index;
+		var mySourceEnd = myDoc.hyperlinks[j].source.sourceText.insertionPoints[-1].index;
+		var myTextBegin = myText.insertionPoints[0].index;
+		var myTextEnd = myText.insertionPoints[-1].index;
+		if ((myDoc.hyperlinks[j].source.sourceText.parent == myText.parent) &&
+			(((myTextBegin >= mySourceBegin) && (myTextBegin <= mySourceEnd)) || ((myTextEnd >= mySourceBegin) && (myTextEnd <= mySourceEnd)))) {
+			myDoc.hyperlinks[j].source.appliedCharacterStyle = myDoc.characterStyles.item("[None]");
+			myDoc.hyperlinks[j].source.remove();
+		}
+	}
 }
