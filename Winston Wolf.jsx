b62746d0214304ad9d434b69cdd204e970e78ea8
markusbjorklund <markus.bjorklund@gmail.com>
2022-11-21 08:51:39 +0200

    fixa mera

--- Winston Wolf.jsx
+++ Winston Wolf.jsx
@@ -134,6 +134,26 @@ function Wolf() {
     app.changeGrepPreferences.changeTo = "\\\x{D}Artikeln har uppdaterats på webben. Kolla så den fortfarande funkar i print.\\\x{D}";
     app.activeDocument.changeGrep();
 
+    app.findGrepPreferences.findWhat = "Uppdaterad (\\d).(\\d)";
+    app.changeGrepPreferences.appliedCharacterStyle = "Bildingress ingång";
+    app.changeGrepPreferences.changeTo = "\\\x{D}Artikeln har uppdaterats på webben. Kolla så den fortfarande funkar i print.\\\x{D}";
+    app.activeDocument.changeGrep();
+
+    app.findGrepPreferences.findWhat = "Uppdaterad (\\d)(\\d).(\\d)";
+    app.changeGrepPreferences.appliedCharacterStyle = "Bildingress ingång";
+    app.changeGrepPreferences.changeTo = "\\\x{D}Artikeln har uppdaterats på webben. Kolla så den fortfarande funkar i print.\\\x{D}";
+    app.activeDocument.changeGrep();
+
+    app.findGrepPreferences.findWhat = "Uppdaterad (\\d)(\\d).(\\d)(\\d)";
+    app.changeGrepPreferences.appliedCharacterStyle = "Bildingress ingång";
+    app.changeGrepPreferences.changeTo = "\\\x{D}Artikeln har uppdaterats på webben. Kolla så den fortfarande funkar i print.\\\x{D}";
+    app.activeDocument.changeGrep();
+
+    app.findGrepPreferences.findWhat = "Uppdaterad (\\d).(\\d)(\\d)";
+    app.changeGrepPreferences.appliedCharacterStyle = "Bildingress ingång";
+    app.changeGrepPreferences.changeTo = "\\\x{D}Artikeln har uppdaterats på webben. Kolla så den fortfarande funkar i print.\\\x{D}";
+    app.activeDocument.changeGrep();
+
     // töm sök och ersätt
     app.findGrepPreferences = NothingEnum.nothing;
     app.changeGrepPreferences = NothingEnum.nothing;
