68afc0e95644382931aa26566f2e3c0410db8167
markusbjorklund <markus.bjorklund@gmail.com>
2022-11-22 15:28:43 +0200

    tato

--- potato.jsx
+++ potato.jsx
@@ -49,48 +49,73 @@
 // author Markus Björklund
 // ------------------------------------
 
-app.findGrepPreferences.findWhat = "Läs också:";
-app.changeGrepPreferences.appliedCharacterStyle = "Bildingress ingång";
-app.changeGrepPreferences.changeTo = "Artikeln har länkade artiklar på webbben. Infoga dessa som hyperlänkar i en webbhänvisning eller ta bort följande stycke.\\\x{D}";
-app.activeDocument.changeGrep();
 
-app.findGrepPreferences.findWhat = "LÄS OCKSÅ:";
-app.changeGrepPreferences.appliedCharacterStyle = "Bildingress ingång";
-app.changeGrepPreferences.changeTo = "Artikeln har länkade artiklar på webbben. Infoga dessa som hyperlänkar i en webbhänvisning eller ta bort följande stycke.\\\x{D}";
-app.activeDocument.changeGrep();
+// main ();
 
-app.findGrepPreferences.findWhat = "Artikeln uppdaterad";
-app.changeGrepPreferences.appliedCharacterStyle = "Bildingress ingång";
-app.changeGrepPreferences.changeTo = "Artikeln har uppdaterats på webben. Kolla så den fortfarande funkar i print.\\\x{D}";
-app.activeDocument.changeGrep();
+// function main() {
+// var grepQuery = "Alkohol.+$";
 
-app.findGrepPreferences = NothingEnum.nothing;
-app.changeGrepPreferences = NothingEnum.nothing;
+// alert(grepQuery);
+
+// //Clear any existing Grep preferences
+// app.findGrepPreferences = NothingEnum.nothing;
+// app.changeGrepPreferences = NothingEnum.nothing;
+
+// //Put the search term into the query
+// app.findGrepPreferences.findWhat = grepQuery;
+
+// //Put the first result of the find into a variable
+// var found_txt = app.activeDocument.findGrep ()[0];
+// //the find operation will return an Array of Text Objects.
+// //by using Javascripts CONTENTS it takes the reult and converts it into a regular String rather than a Text Object.
+// var firstResult = found_txt.contents;
+
+// alert(firstResult);
+
+// //clear the preferences
+// app.findGrepPreferences = NothingEnum.nothing;
+// app.changeGrepPreferences = NothingEnum.nothing;
+
+// //For the lookahead to work it needs something in front of it so I have told it to look for the return.
+// var newQuery = "\\r(?=" + firstResult + ")"
+// app.findGrepPreferences.findWhat = newQuery;
+
+// //Run the find\Change
+// app.activeDocument.changeGrep ();
+
+// //Clear Grep preferences
+// app.findGrepPreferences = NothingEnum.nothing;
+// app.changeGrepPreferences = NothingEnum.nothing;    
+
+// alert("Finished");
+// return;
+// }
+
+
+// var selectedStuff = app.findGrepPreferences.findWhat = "lite text".app.selection[0];
+
+
+//app.selection[0];
+
+// if (selectedStuff instanceof TextFrame) {
+// app.selection = selectedStuff.texts;
+// }
+// app.copy();
+
+
 
 // reset
-app.findGrepPreferences = NothingEnum.nothing;
-app.changeGrepPreferences = NothingEnum.nothing;
+// app.findGrepPreferences = NothingEnum.nothing;
+// app.changeGrepPreferences = NothingEnum.nothing;
 
-// fixa byline byrå på artiklar från TT
-app.findGrepPreferences.findWhat = "\\\x{0028}TT\\\x{0029}";
-app.changeGrepPreferences.changeTo = "TT";
-app.changeGrepPreferences.appliedCharacterStyle = "Byline byrå";
-app.activeDocument.changeGrep();
+// app.findGrep.findWhat = "lite text";
+// app.changeGrep.changeTo = "markör här";
+// app.activeDocument.changeGrep();
 
-app.findGrepPreferences.findWhat = "\\\x{0028}TT-AFP\\\x{0029}";
-app.changeGrepPreferences.changeTo = "TT-AFP";
-app.changeGrepPreferences.appliedCharacterStyle = "Byline byrå";
-app.activeDocument.changeGrep();
+// app.selection[0].contents 
+// app.copy();
 
-app.findGrepPreferences.findWhat = "\\\x{0028}TT-AP\\\x{0029}";
-app.changeGrepPreferences.changeTo = "TT-AP";
-app.changeGrepPreferences.appliedCharacterStyle = "Byline byrå";
-app.activeDocument.changeGrep();
-
-app.findGrepPreferences.findWhat = "\\\x{0028}TT-NTB\\\x{0029}";
-app.changeGrepPreferences.changeTo = "TT-NTB";
-app.changeGrepPreferences.appliedCharacterStyle = "Byline byrå";
-app.activeDocument.changeGrep();
+//app.pasteInPlace();
 
 // reset
 app.findGrepPreferences = NothingEnum.nothing;
@@ -110,15 +135,50 @@ app.changeGrepPreferences = NothingEnum.nothing;
 
 // (?<=“).*?(?=“)
 
-app.findGrepPreferences.findWhat = "<<STATISTIK START>>(?s).*<<STATISTIK SLUT>>";
-app.changeGrepPreferences.appliedParagraphStyle = "Matchfakta";
+// fixa rätt streck mellan resultaten
+app.findGrepPreferences.findWhat = "(\\d)(\\d)-(\\d)(\\d)";
+app.changeGrepPreferences.changeTo = "$1$2–$3$4";
+app.activeDocument.changeGrep();
+
+app.findGrepPreferences.findWhat = "(\\d)(\\d)-(\\d)";
+app.changeGrepPreferences.changeTo = "$1$2–$3";
+app.activeDocument.changeGrep();
+
+app.findGrepPreferences.findWhat = "(\\d)-(\\d)(\\d)";
+app.changeGrepPreferences.changeTo = "$1–$2$3";
+app.activeDocument.changeGrep();
+
+app.findGrepPreferences.findWhat = "(\\d)-(\\d)";
+app.changeGrepPreferences.changeTo = "$1–$2";
 app.activeDocument.changeGrep();
 
 // reset
 app.findGrepPreferences = NothingEnum.nothing;
 app.changeGrepPreferences = NothingEnum.nothing;
 
-app.findGrepPreferences.findWhat = "<<STATISTIK START>>\\\x{000D}";
+app.findGrepPreferences.findWhat = "<<MATCHEN START>>(?s).*<<MATCHEN SLUT>>";
+app.changeGrepPreferences.appliedParagraphStyle = "Gren/Matchvinjett";
+app.activeDocument.changeGrep();
+
+// reset
+app.findGrepPreferences = NothingEnum.nothing;
+app.changeGrepPreferences = NothingEnum.nothing;
+
+app.findGrepPreferences.findWhat = "<<LAGEN START>>(?s).*<<LAGEN SLUT>>";
+app.changeGrepPreferences.appliedParagraphStyle = "Gren/Matchvinjett lag";
+app.activeDocument.changeGrep();
+
+// reset
+app.findGrepPreferences = NothingEnum.nothing;
+app.changeGrepPreferences = NothingEnum.nothing;
+
+app.findGrepPreferences.findWhat = "STATISTIK START(?s).*STATISTIK SLUT";
+//app.changeGrepPreferences.appliedParagraphStyle = "Matchfakta";
+app.changeGrepPreferences.changeTo = "";
+app.activeDocument.changeGrep();
+
+app.findGrepPreferences.findWhat = "STATISTIK SLUT";
+//app.changeGrepPreferences.appliedParagraphStyle = "Matchfakta";
 app.changeGrepPreferences.changeTo = "";
 app.activeDocument.changeGrep();
 
@@ -126,18 +186,149 @@ app.activeDocument.changeGrep();
 app.findGrepPreferences = NothingEnum.nothing;
 app.changeGrepPreferences = NothingEnum.nothing;
 
-app.findGrepPreferences.findWhat = "<<STATISTIK SLUT>>\\\x{000D}";
+// fixa egen rad till periodsiffrorna
+app.findGrepPreferences.findWhat = "\(\\d)( )\\((\\d)";
+app.changeGrepPreferences.changeTo = "$1\\n($3";
+app.activeDocument.changeGrep();
+
+// fixa konsekvent e förl.
+app.findGrepPreferences.findWhat = "e(\\.) förl.";
+app.changeGrepPreferences.changeTo = "e förl.";
+app.activeDocument.changeGrep();
+
+// fixa konsekvent e förl.
+app.findGrepPreferences.findWhat = "e(\\.) förl";
+app.changeGrepPreferences.changeTo = "e förl.ööö";
+app.activeDocument.changeGrep();
+
+// fixa konsekvent e str.
+app.findGrepPreferences.findWhat = "e(\\.) str.";
+app.changeGrepPreferences.changeTo = "e str.";
+app.activeDocument.changeGrep();
+
+// fixa konsekvent e str.
+app.findGrepPreferences.findWhat = "e(\\.) str";
+app.changeGrepPreferences.changeTo = "e str.";
+app.activeDocument.changeGrep();
+
+// fixa egen rad till periodsiffrorna efter förl.
+app.findGrepPreferences.findWhat = "\(\\.)( )\\((\\d)";
+app.changeGrepPreferences.changeTo = "$1\\n($3";
+app.activeDocument.changeGrep();
+
+// fixa shift-tabben x-x
+app.findGrepPreferences.findWhat = "(\\w) (\\d)–(\\d)(\\n)";
+app.changeGrepPreferences.changeTo = "$1\\\x{0008}$2–$3\n";
+app.activeDocument.changeGrep();
+
+// fixa shift-tabben xx-x
+app.findGrepPreferences.findWhat = "(\\w) (\\d)(\\d)–(\\d)(\\n)";
+app.changeGrepPreferences.changeTo = "$1\\\x{0008}$2$3–$4\n";
+app.activeDocument.changeGrep();
+
+// fixa shift-tabben xx-xx
+app.findGrepPreferences.findWhat = "(\\w) (\\d)(\\d)–(\\d)(\\d)(\\n)";
+app.changeGrepPreferences.changeTo = "$1\\\x{0008}$2$3–$4$5\n";
+app.activeDocument.changeGrep();
+
+// fixa shift-tabben x-xx
+app.findGrepPreferences.findWhat = "(\\w) (\\d)–(\\d)(\\d)(\\n)";
+app.changeGrepPreferences.changeTo = "$1\\\x{0008}$2–$3$4\n";
+app.activeDocument.changeGrep();
+
+// fixa shift-tabben x-x förl.
+app.findGrepPreferences.findWhat = "(\\w) (\\d)–(\\d) e förl.(\\n)";
+app.changeGrepPreferences.changeTo = "$1\\\x{0008}$2–$3 e förl.\n";
+app.activeDocument.changeGrep();
+
+// fixa shift-tabben xx-x förl.
+app.findGrepPreferences.findWhat = "(\\w) (\\d)(\\d)–(\\d) e förl.(\\n)";
+app.changeGrepPreferences.changeTo = "$1\\\x{0008}$2$3–$4 e förl.\n";
+app.activeDocument.changeGrep();
+
+// fixa shift-tabben xx-xx förl.
+app.findGrepPreferences.findWhat = "(\\w) (\\d)(\\d)–(\\d)(\\d) e förl.(\\n)";
+app.changeGrepPreferences.changeTo = "$1\\\x{0008}$2$3–$4$5 e förl.\n";
+app.activeDocument.changeGrep();
+
+// fixa shift-tabben x-xx förl.
+app.findGrepPreferences.findWhat = "(\\w) (\\d)–(\\d)(\\d) e förl.(\\n)";
+app.changeGrepPreferences.changeTo = "$1\\\x{0008}$2–$3$4 e förl.\n";
+app.activeDocument.changeGrep();
+
+// fixa shift-tabben xx-x str.
+app.findGrepPreferences.findWhat = "(\\w) (\\d)(\\d)–(\\d) e str.(\\n)";
+app.changeGrepPreferences.changeTo = "$1\\\x{0008}$2$3–$4 e str.\n";
+app.activeDocument.changeGrep();
+
+// fixa shift-tabben x-x str.
+app.findGrepPreferences.findWhat = "(\\w) (\\d)–(\\d) e str.(\\n)";
+app.changeGrepPreferences.changeTo = "$1\\\x{0008}$2–$3 e str.\n";
+app.activeDocument.changeGrep();
+
+// fixa shift-tabben xx-xx str.
+app.findGrepPreferences.findWhat = "(\\w) (\\d)(\\d)–(\\d)(\\d) e str.(\\n)";
+app.changeGrepPreferences.changeTo = "$1\\\x{0008}$2$3–$4$5 e str.\n";
+app.activeDocument.changeGrep();
+
+// fixa shift-tabben x-xx str.
+app.findGrepPreferences.findWhat = "(\\w) (\\d)–(\\d)(\\d) e str.(\\n)";
+app.changeGrepPreferences.changeTo = "$1\\\x{0008}$2–$3$4 e str.\n";
+app.activeDocument.changeGrep();
+
+// reset
+app.findGrepPreferences = NothingEnum.nothing;
+app.changeGrepPreferences = NothingEnum.nothing;
+
+app.findGrepPreferences.findWhat = "<<MATCHEN START>>";
 app.changeGrepPreferences.changeTo = "";
 app.activeDocument.changeGrep();
 
 // reset
 app.findGrepPreferences = NothingEnum.nothing;
 app.changeGrepPreferences = NothingEnum.nothing;
+
+app.findGrepPreferences.findWhat = "<<MATCHEN SLUT>>";
+app.changeGrepPreferences.changeTo = "";
+app.activeDocument.changeGrep();
+
+// reset
+app.findGrepPreferences = NothingEnum.nothing;
 app.changeGrepPreferences = NothingEnum.nothing;
 
-app.findGrepPreferences.findWhat = " \\\<\\\d";
+//app.findGrepPreferences.findWhat = "<<STATISTIK SLUT>>\\\x{000D}";
+app.findGrepPreferences.findWhat = "<<LAGEN START>>";
+app.changeGrepPreferences.changeTo = "";
+app.activeDocument.changeGrep();
+
+// reset
+app.findGrepPreferences = NothingEnum.nothing;
+app.changeGrepPreferences = NothingEnum.nothing;
+
+app.findGrepPreferences.findWhat = "<<LAGEN SLUT>>";
+app.changeGrepPreferences.changeTo = "";
+app.activeDocument.changeGrep();
+
+// reset
+app.findGrepPreferences = NothingEnum.nothing;
+app.changeGrepPreferences = NothingEnum.nothing;
+
+app.findGrepPreferences.findWhat = "<<STATISTIK START>>";
+app.changeGrepPreferences.changeTo = "";
+app.activeDocument.changeGrep();
+
+app.findGrepPreferences.findWhat = "<<STATISTIK SLUT>>";
+app.changeGrepPreferences.changeTo = "";
+app.activeDocument.changeGrep();
+
+// reset
+app.findGrepPreferences = NothingEnum.nothing;
+app.changeGrepPreferences = NothingEnum.nothing;
+
+/*app.findGrepPreferences.findWhat = " \\\<\\\d";
 app.changeGrepPreferences.changeTo = "första siffran";
 app.activeDocument.changeGrep();
+*/
 
 //~ app.findGrepPreferences.findWhat = "<<GREN/DIVISION>>(?s).*<<LAGEN>>";
 //~ app.changeGrepPreferences.appliedParagraphStyle = "Matchvinjett";
